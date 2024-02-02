76f7474d792db87eacae55fa8634287108f7723b
Corey Guercio <72572116+cguercio@users.noreply.github.com>
2024-02-02 08:58:31 -0600

    Update graphics

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import GUI
+from graphics import gui
 
 WIDTH, HEIGHT = 800, 800
 
 def main():
-    screen = GUI.Screen(WIDTH, HEIGHT)
-    screen.display()
+    running = True
+
+    while running:
+
+        screen = gui.Screen(WIDTH, HEIGHT)
+        screen.display()
+
+        for event in gui.pygame.event.get():
+            if event.type == gui.pygame.QUIT:
+                running = False
+                break
 
 if __name__ == '__main__':
     main()
