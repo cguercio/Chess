83af076e069f949585ce7715e13ed554a036757c
Corey Guercio <cvguercio@gmail.com>
2024-02-07 08:02:28 -0600

    Quick commit to main before new branch

--- utils/utils.py
+++ utils/utils.py
@@ -11,10 +11,11 @@ def wait():
             
 # This function converts the mouse position to the position of the board.
 def mouse_pos_to_board_pos(pos, board):
-    num_cols = len(board[0]) # Gets the number of cols by geting the length of the first list
+    
+    num_cols = len(board[0]) # Gets the number of cols by getting the length of the first list
     num_rows = len(board) # Gets the number of rows by getting the number of lists
+    square_width = WIDTH // num_cols
+    square_height = HEIGHT // num_rows
 
-    new_posisition = (int(math.floor(pos[0] // (WIDTH // num_cols))),
-                       int(math.floor(pos[1] // (HEIGHT // num_rows))))
+    return (int(math.floor(pos[0] // square_width)), int(math.floor(pos[1] // square_height)))
 
-    return new_posisition
